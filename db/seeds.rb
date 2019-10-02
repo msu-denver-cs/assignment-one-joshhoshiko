a5fd5140254a28ce2aeece25e4e6be8cbe7eefb8
joshhoshiko <jhoshiko@msudenver.edu>
2019-10-01 21:07:59 -0600

    Added seed_dump. Updated Seed File
    Installed the seed_dump gem and used it to create a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Make.create!([
+  {name: "Toyota", country: "United States"},
+  {name: "Mazda", country: "Japan"},
+  {name: "Mercedes", country: "Germany"},
+  {name: "Chevy", country: "United States"},
+  {name: "Bugatti", country: "France"}
+])
+Part.create!([
+  {name: "Exhaust"},
+  {name: "Turbo"},
+  {name: "Shocks"},
+  {name: "Muffler"},
+  {name: "Axle"},
+  {name: "Windshield Wipers"}
+])
